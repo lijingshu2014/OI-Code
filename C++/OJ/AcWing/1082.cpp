49b5d7fce9ca67ce20bc1bebf820ad8cf75b929e
lijingshu2014 <3694003747@qq.com>
2025-11-29 12:01:45 +0800

    添加 AcWing 数位 DP 的题目

---
+++ C++/OJ/AcWing/1082.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 15;
+int f[N][N];  // f[i, j] 表示一共有 i 位，且最高位填 j 的数的个数
+void init() {
+    for (int i = 0; i <= 9; i++)
+        f[1][i] = 1;
+    for (int i = 2; i < N; i++)
+        for (int j = 0; j <= 9; j++)
+            for (int k = j; k <= 9; k++)
+                f[i][j] += f[i - 1][k];
+}
+int dp(int n) {
+    if (!n)
+        return 1;
+    vector< int > nums;
+    while (n)
+        nums.push_back(n % 10), n /= 10;
+    int res = 0;
+    int last = 0;
+    for (int i = nums.size() - 1; i >= 0; i--) {
+        int x = nums[i];
+        for (int j = last; j < x; j++)
+            res += f[i + 1][j];
+        if (x < last)
+            break;
+        last = x;
+        if (!i)
+            res++;
+    }
+    return res;
+}
+int main() {
+    init();
+    int l, r;
+    while (cin >> l >> r)
+        cout << dp(r) - dp(l - 1) << endl;
+    return 0;
+}
